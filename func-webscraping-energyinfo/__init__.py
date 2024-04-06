d03d87b6084276ca2292991ac2ae06d8782fc584
BigTeddyRush <julian.britz@web.de>
2024-04-06 12:16:51 +0200

    test MI

--- func-webscraping-energyinfo/__init__.py
+++ func-webscraping-energyinfo/__init__.py
@@ -53,11 +53,11 @@ def webscraping():
     # create dict
     power_consumption_dict = {'unix_seconds' : unix_seconds_list, 'name' : name_list, 'data' : data_list}
 
-    server = 'energycharts.database.windows.net'
-    database = 'EnergyChartsDB'
-    username = 'bigteddyrush'
-    password = '{qepniZ-tyhxus-3pubmu}'
-    driver = '{ODBC Driver 17 for SQL Server}'
+    #server = 'energycharts.database.windows.net'
+    #database = 'EnergyChartsDB'
+    #username = 'bigteddyrush'
+    #password = '{qepniZ-tyhxus-3pubmu}'
+    #driver = '{ODBC Driver 17 for SQL Server}'
 
     connection_string = os.environ["AZURE_SQL_CONNECTIONSTRING"]
 
