9326a82f7dd2eb9443eccabf19abd80ac1beb0c1
BigTeddyRush <82336030+BigTeddyRush@users.noreply.github.com>
2024-04-10 13:01:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,22 +26,25 @@ try:
                 power_consumption_dict['unix_seconds'].append(unix_seconds)
                 power_consumption_dict['name'].append(name)
                 power_consumption_dict['data'].append(data)
-except pyodbc.Error as ex:
+            data = power_consumption_dict
+
+            df = pd.DataFrame(data)
+            
+            # Streamlit-App-Layout definieren
+            st.title('Visualisierung des DataFrames')
+            
+            # DataFrame anzeigen
+            st.write(df)
+            
+            # Plot erstellen
+            fig = px.line(df, x='unix_seconds', y='data', color='name', title='Hydro pumped storage consumption über die Zeit')
+            fig.update_layout(xaxis_title='Zeit', yaxis_title='Verbrauch')
+            st.plotly_chart(fig)
+
+except:
     print("Fehler beim Verbinden zur Datenbank:", ex)
+    # Streamlit-App-Layout definieren
+    st.title('Verbindung zur Datenbank fehlgeschlagen')
 
 ################################
 
-data = power_consumption_dict
-
-df = pd.DataFrame(data)
-
-# Streamlit-App-Layout definieren
-st.title('Visualisierung des DataFrames')
-
-# DataFrame anzeigen
-st.write(df)
-
-# Plot erstellen
-fig = px.line(df, x='unix_seconds', y='data', color='name', title='Hydro pumped storage consumption über die Zeit')
-fig.update_layout(xaxis_title='Zeit', yaxis_title='Verbrauch')
-st.plotly_chart(fig)
